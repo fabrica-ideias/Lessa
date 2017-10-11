713e6f0fbeb654b3d2d34c2e2a8e7c027105fef8
emerson <emerson.jx13@gmail.com>
2017-10-11 06:52:52 -0300

    NOVO AUTOCOMPLETE

--- www/js/util.js
+++ www/js/util.js
@@ -51,3 +51,59 @@ function formataCampo(campo, Mascara, evento) {
         return true;
     }
 }
+function autocompleteeditado(autocomplete,states) {
+    $(function () {
+        var alreadyFilled = false;
+        function initDialog() {
+            clearDialog();
+            for (var i = 0; i < states.length; i++) {
+                $('.dialog').append('<div id="'+states[i].value+'">' + states[i].text + '</div>');
+            }
+        }
+
+        function clearDialog() {
+            $('.dialog').empty();
+        }
+
+        $(autocomplete+' input').click(function () {
+            if (!alreadyFilled) {
+                $('.dialog').addClass('open');
+            }
+
+        });
+        $('body').on('click', '.dialog div', function () {
+            $(autocomplete+' input').val($(this).text()).focus();
+            $(autocomplete+' .close').addClass('visible');
+            alert(this.id);
+            alreadyFilled = true;
+        });
+        $(autocomplete+' .close').click(function () {
+            alreadyFilled = false;
+            $('.dialog').addClass('open');
+            $(autocomplete+' input').val('').focus();
+            $(this).removeClass('visible');
+        });
+
+        function match(str) {
+            str = str.toLowerCase();
+            clearDialog();
+            for (var i = 0; i < states.length; i++) {
+                if (states[i].text.toLowerCase().startsWith(str)) {
+                    $('.dialog').append('<div id="'+states[i].value+'">' + states[i].text + '</div>');
+                }
+            }
+        }
+
+        $(autocomplete+' input').on('input', function () {
+            $('.dialog').addClass('open');
+            alreadyFilled = false;
+            match($(this).val());
+        });
+        $('body').click(function (e) {
+            if (!$(e.target).is("input, .close")) {
+                $('.dialog').removeClass('open');
+            }
+        });
+        initDialog();
+    });
+}
